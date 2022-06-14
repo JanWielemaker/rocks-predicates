9c1d229fc8ed53f345977461d22d73b44b98b1bd
Jan Wielemaker <J.Wielemaker@vu.nl>
2022-06-14 11:22:30 +0200

    Initial files

---
+++ README.md
@@ -0,0 +1,10 @@
+# K/V store based predicates
+
+## Head representation
+
+  - Pred -> Pred-no
+  - Clause-No --> Clause
+  - Indexes
+    - Pred-no+arg --> list of clauses
+
+Or, pred/db?
---
+++ rocks_preds.pl
@@ -0,0 +1,121 @@
+:- module(rocks_preds,
+          [ rdb_open/1,                 % +Directory
+            rdb_assertz/1,              % +Clause
+            rdb_clause/2                % +Head,-Body
+          ]).
+:- use_module(library(rocksdb)).
+:- use_module(library(prolog_code)).
+
+/** <module> Store full predicates in a RocksDB
+
+Databases:
+
+  - Term --> id
+  - S,P  --> O
+
+
+*/
+
+:- meta_predicate
+    rdb_assertz(:),
+    rdb_clause(:, -).
+
+
+:- dynamic
+    intern_table/1,
+    extern_table/1,
+    triple_table/1.
+
+%!  rdb_assertz(+Clause) is det.
+%
+%   Store a clause in the persistent database
+
+rdb_assertz(Clause) :-
+    clause_head_body(Clause, Head, Body),
+    pi_head(PI, Head),
+    t(PI, last_clause_id, Id),
+    NId is Id+1,
+    put(PI, last_clause_id, NId),
+    put(NId, value, (Head :- Body)).
+
+%!  rdb_clause(+Head, -Body) is nondet.
+%
+%   Retrieve a clause from the persistent database
+
+rdb_clause(Head, Body) :-
+    pi_head(PI, Head),
+    t(PI, last_clause_id, LastId),
+    between(1, LastId, ClauseNo),
+    t(ClauseNo, clause, (Head, Body)).
+
+clause_head_body((Head0 :- Body0), Head, Body) =>
+    Head = Head0,
+    Body = Body0.
+clause_head_body(Head0, Head, Body) =>
+    Head = Head0,
+    Body = true.
+
+
+		 /*******************************
+		 *        TRIPLE DATABASE	*
+		 *******************************/
+
+t(S,P,O) :-
+    intern(S, Sid),
+    intern(P, Pid),
+    tid(Sid, Pid, OId),
+    extern(OId, O).
+
+put(S,P,O) :-
+    intern(S, Sid),
+    intern(P, Pid),
+    intern(O, Oid),
+    s_p_sp(Sid, Pid, SP),
+    triple_table(DB),
+    rocks_put(DB, SP, Oid).
+
+intern(Term, Id) :-
+    intern_table(DB),
+    (   rocks_get(DB, Term, Id0)
+    ->  Id = Id0
+    ;   extern_table(EDB),
+        rocks_get(DB, '$$LastID', Id0),
+        Id is Id0+1,
+        rocks_put(DB, '$$LastID', Id),
+        rocks_put(DB, Term, Id),
+        rocks_put(EDB, Id, Term)
+    ).
+
+extern(Id, Term) :-
+    extern_table(EDB),
+    rocks_get(EDB, Id, Term).
+
+tid(S,P,O) :-
+    triple_table(DB),
+    s_p_sp(S, P, SP),
+    rocks_get(DB, SP, O).
+
+s_p_sp(S,P,SP) :-
+    SP is S<<40+P.
+
+%!  rdb_open(+Directory)
+%
+%   Open a database for persistent predicates
+
+rdb_open(Dir) :-
+    ensure_directory(Dir),
+    directory_file_path(Dir, intern, InternName),
+    directory_file_path(Dir, extern, ExternName),
+    directory_file_path(Dir, triple, TripleName),
+    rocks_open(InternName, IDB, [ key(term), value(int64) ]),
+    rocks_open(ExternName, EDB, [ key(int64), value(term) ]),
+    rocks_open(TripleName, TDB, [ key(int64), value(int64) ]),
+    asserta(intern_table(IDB)),
+    asserta(extern_table(EDB)),
+    asserta(triple_table(TDB)).
+
+ensure_directory(Dir) :-
+    exists_directory(Dir),
+    !.
+ensure_directory(Dir) :-
+    make_directory(Dir).
