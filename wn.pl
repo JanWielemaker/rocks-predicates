a5c691479847bee43f974e79a4202e36ecb69a2b
Jan Wielemaker <J.Wielemaker@vu.nl>
2022-06-15 16:30:57 +0200

    Better demo.

--- wn.pl
+++ wn.pl
@@ -6,31 +6,46 @@ user:file_search_path(wn, '/home/janw/3rdparty/Wordnet-3.0/prolog').
     rdb_open('wn.db').
 
 wn_load :-
-    forall(wn_file(File),
+    forall(wn_file(File, _Pred),
            wn_load(File)).
 
 wn_load(File) :-
     rdb_load_file(wn(File)).
 
-wn_file(wn_ant).
-wn_file(wn_at).
-wn_file(wn_cls).
-wn_file(wn_cs).
-wn_file(wn_der).
-wn_file(wn_ent).
-wn_file(wn_fr).
-wn_file(wn_g).
-wn_file(wn_hyp).
-wn_file(wn_ins).
-wn_file(wn_mm).
-wn_file(wn_mp).
-wn_file(wn_ms).
-wn_file(wn_per).
-wn_file(wn_ppl).
-wn_file(wn_sa).
-wn_file(wn_sim).
-wn_file(wn_sk).
-wn_file(wn_s).
-wn_file(wn_syntax).
-wn_file(wn_vgp).
+wn_file(wn_ant,    ant/4).
+wn_file(wn_at,     at/2).
+wn_file(wn_cls,    cls/5).
+wn_file(wn_cs,     cs/2).
+wn_file(wn_der,    der/4).
+wn_file(wn_ent,    ent/2).
+wn_file(wn_fr,     fr/3).
+wn_file(wn_g,      g/2).
+wn_file(wn_hyp,    hyp/2).
+wn_file(wn_ins,    ins/2).
+wn_file(wn_mm,     mm/2).
+wn_file(wn_mp,     mp/2).
+wn_file(wn_ms,     ms/2).
+wn_file(wn_per,    per/4).
+wn_file(wn_ppl,    ppl/4).
+wn_file(wn_sa,     sa/4).
+wn_file(wn_sim,    sim/2).
+wn_file(wn_sk,     sk/3).
+wn_file(wn_s,      s/6).
+wn_file(wn_syntax, syntax/3).
+wn_file(wn_vgp,    vgp/4).
 
+index :-
+    rdb_index(hyp/2, 1).
+
+hyp(A1,A2) :-
+    rdb_clause(hyp(A1,A2), true).
+
+hyp(A1) :-
+    hyp(A1, _), !.
+
+thyp :-
+    findall(A1, hyp(A1,_), List),
+    length(List, N),
+    format('~D clauses~n', [N]),
+    random_permutation(List, Tests),
+    time(maplist(hyp, Tests)).
